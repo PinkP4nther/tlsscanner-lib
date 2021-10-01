3100ca9a4bab2ac525661d39588ff6e4e4ba3279
PinkP4nther <0x0090@protonmail.com>
2021-09-30 22:09:46 -0700

    Updated example scan.

--- examples/scan/src/main.rs
+++ examples/scan/src/main.rs
@@ -6,12 +6,19 @@ use std::time::Instant;
 
 fn main() {
 
+    // Check args
+    let args: Vec<String> = env::args().into_iter().collect();
+    if args.len() < 4 {
+        println!("[!] {} <HOST> <PORT> <CN>", args[0]);
+        std::process::exit(-1);
+    }
+
     // Start counting time
     let counter = Instant::now();
 
     // Scan host and store results
     // (TCP Host, TCP Port, CN)
-    let scan_results = TlsScanner("google.com", "443", "google.com").scan();
+    let scan_results = TlsScanner(args[1].as_str(), args[2].as_str(), args[3].as_str()).scan();
 
     // Get elapsed time.
     let elapsed_seconds = counter.elapsed().as_millis();
@@ -25,6 +32,6 @@ fn main() {
     println!("[+] TLS 1.3: {}", scan_results.tls13);
 
     // Print elapsed time.
-    println!("[+] Time elapsed: {} ms.", elapsed_seconds);
+    println!("[+] Host scanned: {}\n[+] Time elapsed: {} ms.", args[1], elapsed_seconds);
 }
 
